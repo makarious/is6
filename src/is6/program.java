64997a43b7df3965d041e312d0db4c3bcc983979
user <user@172.31.44.212>
2016-03-23 16:46:21 +0200

    adde new welcom

--- src/is6/program.java
+++ src/is6/program.java
@@ -4,7 +4,12 @@ public class program {
 
 	public static void main(String[] args) {
 		System.out.println("helloo world");
+		welcom();
 
+	}
+	public static void welcom()
+	{
+		System.out.println("helloo world");
 	}
 
 }
